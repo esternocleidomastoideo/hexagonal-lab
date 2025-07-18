46fb171a13543141657646f255dfb4957351c95c
marceloaraujo14hotmail.com <marceloaraujo14@hotmail.com>
2025-07-17 21:42:26 -0300

    first commit

---
+++ src/main/java/com/araujo/hexagonal_sample/HexagonalSampleApplication.java
@@ -0,0 +1,13 @@
+package com.araujo.hexagonal_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/araujo/hexagonal_sample/HexagonalSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araujo.hexagonal_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
